6ae72182443fa3003d08454c6fb7057b49781df0
William <s3864077@rmit.edu.vn>
2021-06-11 15:25:35 +0700

    Etou first commit

---
+++ src/com/company/Contact.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Contact {
+    public String getContactName() {
+        return ContactName;
+    }
+
+    public void setContactName(String contactName) {
+        ContactName = contactName;
+    }
+
+    public String getContactPhoneNum() {
+        return ContactPhoneNum;
+    }
+
+    public void setContactPhoneNum(String contactPhoneNum) {
+        ContactPhoneNum = contactPhoneNum;
+    }
+
+    private String ContactName;
+    private String ContactPhoneNum;
+
+}
---
+++ src/com/company/MobilePhone.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import java.util.ArrayList;
+
+public class MobilePhone {
+    private ArrayList<Contact> ContactList = new ArrayList<Contact>();
+    public static void store(){
+
+    }
+    public static void modify(){
+
+    }
+    public static void remove(){
+
+    }
+    public static void query(){
+
+    }
+
+}
